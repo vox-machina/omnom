9e3daf500f5f9d1823d65d2c1448688c5e051438
Ross McDonald <85923+rossajmcd@users.noreply.github.com>
2024-02-15 14:10:13 +0000

    add handling for IO exceptions

--- src/omnom/generators/file.clj
+++ src/omnom/generators/file.clj
@@ -4,13 +4,20 @@
             [clojure.java.io :refer [file reader]]
             [clojure.string :refer [includes?]]))
 
-(defn pedestal-log->events
+(defn with-io-exists-xs-handling
+  "Handles cases where FileNotFoundException arises by returning empty collection"
+  [f] (println "handling exceptions in wrapper") (fn [& args] (try (apply f args) (catch Exception e []))))
+
+(defn pedestal-log->es!
   "Generate log file lines given a path.
    Filter using ns-filter e.g. to include onlines that relate to a namespace.
    Split on a given token to create events out of log lines."
   [path ns-filter token]
-  (try (->> (line-seq (reader (file path)))
+  (->> (line-seq (reader (file path)))
        (filter #(includes? % ns-filter))
        (map #(.split % token))
-       (map #(hash-map :ns ns-filter :instant (first (.split (first %) " ")) :log (read-string (second %)))))
-       (catch Exception e [])))
+       (map #(hash-map :ns ns-filter :instant (first (.split (first %) " ")) :log (read-string (second %))))))
+
+(def pedestal-log->es
+  "Wrap pedestal-log->es to return empty collection if file does not exist."
+  (with-io-exists-xs-handling pedestal-log->es!))
